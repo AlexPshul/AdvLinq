533176af7bc5378e88b0b771d74e1fb1d8d33aaf
Alex Pshul <alex@pshul.com>
2017-07-09 00:44:20 +0300

    Added Test.Core project

---
+++ Test.Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using AdvLinq;
+using System;
+using System.Linq;
+
+namespace Test.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            Enumerable
+                .Range(1, 100)
+                .Select(_ => r.Next(1, 100))
+                .SelectIf(number => number > 50, _ => "Bigger", _ => "Smaller")
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
